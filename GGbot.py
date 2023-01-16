011572a8c71187125d80d9a2e95feceb8f5278f9
LiiJoee <n.oparin95@gmail.com>
2023-01-16 15:24:47 +0300

    From and last name

--- GGbot.py
+++ GGbot.py
@@ -1,5 +1,11 @@
 import telebot
 
 bot=telebot.TeleBot('5709307077:AAErb5ut5IKM7xHJh1LxncPB9bexG8DwVac')
-telebot
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Привет <b>{message.from_user.first_name } <u>{message.from_user.last_name}</u></b>'
+    bot. send_message(message.chat.id, '<b>Hi</b>', parse_mode='html')
+
+
+bot.polling(none_stop=True)
