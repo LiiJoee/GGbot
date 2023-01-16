c9541d1d5256fd81166fd488371da92d8df8653c
LiiJoee <n.oparin95@gmail.com>
2023-01-16 15:41:10 +0300

    message

--- GGbot.py
+++ GGbot.py
@@ -4,8 +4,12 @@ bot=telebot.TeleBot('5709307077:AAErb5ut5IKM7xHJh1LxncPB9bexG8DwVac')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess = f'Привет <b>{message.from_user.first_name } <u>{message.from_user.last_name}</u></b>'
-    bot. send_message(message.chat.id, '<b>Hi</b>', parse_mode='html')
+    mess = f'Привет, <b>{message.from_user.first_name } <u>{message.from_user.last_name}</u></b>'
+    bot. send_message(message.chat.id, mess, parse_mode='html')
 
+
+@bot.message_handler()
+def get_user_text(message):
+    bot.send_message(message.chat.id, message, parse_mode='html')
 
 bot.polling(none_stop=True)
